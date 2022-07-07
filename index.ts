9513b59b182e299e216852a1684b6e28a2610312
Account-Personal <494671601a@gmail.com>
2022-07-07 15:29:56 +0200

    add timer

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import * as dotenv from 'dotenv';
 import { app } from './Route';
 import { mediate } from './middleware/middlewareMediator'
-
+import * as schedule from 'node-schedule'
+import {DateTime} from 'luxon';
 
 dotenv.config()
-mediate()
+mediate();
+const job = schedule.scheduleJob({hour:21,minutes:0,second:30}, function(){
+    console.log(DateTime.now().toISO());
+    console.log('timer run');
+  });
 console.log("Initialization complete");
