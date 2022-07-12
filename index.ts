42e775979ed15dec1d3bf7d6e0591a0fdeaa5b5b
Leo <494671601a@gmail.com>
2022-07-12 18:11:29 +0200

    ok

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import * as dotenv from 'dotenv';
 import { mediate } from './middleware/middlewareMediator';
 import {startJob} from './job/dailyjob';
-import { DBConnection } from './config/sequelize';
 
 dotenv.config();
 mediate();
