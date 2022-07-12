31241b1d5d0c9a053b14f3240f50c1b5ab4bf759
Leo <494671601a@gmail.com>
2022-07-12 18:10:58 +0200

    remove console log

--- index.ts
+++ index.ts
@@ -4,8 +4,6 @@ import {startJob} from './job/dailyjob';
 import { DBConnection } from './config/sequelize';
 
 dotenv.config();
-console.log("enviroment variable:");
-console.log(process.env);
 DBConnection.getInstance().getConnection();
 mediate();
 startJob();
